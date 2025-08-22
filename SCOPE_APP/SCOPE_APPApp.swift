0bfcfb5b77ee0b93793d6494439874cf5159645a
AshutoshSri123 <ashu.61003@gmail.com>
2025-08-23 01:38:51 +0530

    Initial Commit

---
+++ SCOPE_APP/SCOPE_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCOPE_APPApp.swift
+//  SCOPE_APP
+//
+//  Created by Ashutosh Srivastava on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SCOPE_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SCOPE_APPTests/SCOPE_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  SCOPE_APPTests.swift
+//  SCOPE_APPTests
+//
+//  Created by Ashutosh Srivastava on 8/23/25.
+//
+
+import Testing
+@testable import SCOPE_APP
+
+struct SCOPE_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
